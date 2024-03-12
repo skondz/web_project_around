6d5218f2c00ff6e7fcaa57142f1d3fdecf25b33d
Skondz <skondz@gmail.com>
2024-03-11 18:25:57 -0600

    dev: js sprint 6

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,8 +1,6 @@
-const profileElement = document.querySelector(".profile");
-const profileNameElement = profileElement.querySelector(".profile__info");
-const profileTitleElement = profileElement.querySelector(".profile__title");
-
-const openPopupButton = profileElement.querySelector(".profile__edit");
+const profileNameElement = document.querySelector(".profile__info");
+const profileTitleElement = document.querySelector(".profile__title");
+const openPopupButton = document.querySelector(".profile__edit");
 const popupElement = document.querySelector(".popup");
 const closePopupButton = popupElement.querySelector(".popup__close-button");
 const savePopupButton = popupElement.querySelector(".popup__save-button");
@@ -15,18 +13,42 @@ function openPopup() {
   inputName.value = profileNameElement.textContent;
   inputTitle.value = profileTitleElement.textContent;
 }
-
 function closePopup() {
   popupElement.classList.remove("popup__opened"); //cierra el popup
 }
-
 function saveChanges(event) {
   event.preventDefault();
   profileNameElement.textContent = inputName.value;
   profileTitleElement.textContent = inputTitle.value;
   popupElement.classList.remove("popup__opened"); //cierra el popup
 }
-
 openPopupButton.addEventListener("click", openPopup);
 closePopupButton.addEventListener("click", closePopup);
 formElement.addEventListener("submit", saveChanges);
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
