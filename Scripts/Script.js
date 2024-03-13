60965c82f7da2fa0a712b2f1effcea86c241bc16
Skondz <skondz@gmail.com>
2024-03-13 17:40:30 -0600

    fix Java

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,61 +1,26 @@
-const profileNameElement = document.querySelector(".profile__info");
-const profileTitleElement = document.querySelector(".profile__title");
-const openPopupButton = document.querySelector(".profile__edit");
-const popupElement = document.querySelector(".popup");
-const profileElement = document.querySelector(".popup-profile");
-const addElement = document.querySelector(".popup-add");
-const closePopupButton = popupElement.querySelector(".popup__close-button");
-const savePopupButton = popupElement.querySelector(".popup__button-save");
-const openAddButton = document.querySelector(".profile__add");
-const inputName = popupElement.querySelector(".popup__input-name");
-const inputTitle = popupElement.querySelector(".popup__input-title");
-const formElement = popupElement.querySelector(".popup__form");
+const popUp = document.querySelector("#popup-profile");
+const btnEdit = document.querySelector(".profile__edit");
+const btnClose = document.querySelector(".popup__close-button");
+const profileNameInput = document.querySelector("#input-name");
+const profileAboutInput = document.querySelector("#input-title");
+const profileName = document.querySelector(".profile__info");
+const profileAbout = document.querySelector(".profile__title");
+const btnProfileSave = document.querySelector(".popup__button-save");
+const formProfile = document.querySelector(".popup__form");
+// abrir popup profile
+function openClosePopup() {
+  popUp.classList.toggle("popup__show");
+  profileNameInput.value = profileName.textContent;
+  profileAboutInput.value = profileAbout.textContent;
+}
+// cambiar profile
+function saveChanges(evt) {
+  evt.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileAbout.textContent = profileAboutInput.value;
+  openPopUp();
+}
 
-function openProfilePopup() {
-  profileElement.classList.toggle("popup__opened"); //abre el popup
-  inputName.value = profileNameElement.textContent;
-  inputTitle.value = profileTitleElement.textContent;
-}
-function openAddPopup() {
-  addElement.classList.toggle("popup__opened"); //abre el popup
-}
-function closeProfilePopup() {
-  popupElement.classList.remove("popup__opened"); //cierra el popup
-}
-function saveChanges(event) {
-  event.preventDefault();
-  profileNameElement.textContent = inputName.value;
-  profileTitleElement.textContent = inputTitle.value;
-  popupElement.classList.remove("popup__opened"); //cierra el popup
-}
-openPopupButton.addEventListener("click", openProfilePopup);
-closePopupButton.addEventListener("click", closeProfilePopup);
-openAddButton.addEventListener("click", openAddPopup);
-formElement.addEventListener("submit", saveChanges);
-
-const initialCards = [
-  {
-    name: "Valle de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
-  },
-  {
-    name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
-  },
-  {
-    name: "Montañas Calvas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
-  },
-  {
-    name: "Parque Nacional de la Vanoise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
-  },
-];
+btnEdit.addEventListener("click", openClosePopup);
+btnClose.addEventListener("click", openClosePopup);
+formProfile.addEventListener("submit", saveChanges);
